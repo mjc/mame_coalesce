df669ef99daeb215399f9ab39bbb2278277ae582
Michael J. Cohen <mjc@kernel.org>
2019-08-01 22:51:49 -0600

    Don't open zip til after we are sure there is at least one file to add, also listen to clippy.

--- src/rom/zip.rs
+++ src/rom/zip.rs
@@ -1,6 +1,6 @@
 use crate::rayon::prelude::*;
-use crate::zip::write::{ZipWriter, FileOptions};
 use crate::rom;
+use crate::zip::write::{FileOptions, ZipWriter};
 
 use std::fs;
 use std::path::{Path, PathBuf};
@@ -11,23 +11,36 @@ pub fn write_zip(bundle: &rom::Bundle, zip_dest: PathBuf) {
     let path: PathBuf = [zip_dest.to_str().unwrap(), output_file_name.as_str()]
         .iter()
         .collect();
-    let output = fs::File::create(path).unwrap();
-    let mut zip = ZipWriter::new(output);
-    bundle.files.iter().for_each(|(sha, _file)| {
-        match bundle
-            .matches
-            .iter()
-            .find(|(sha1, _dest, _src)| sha == sha1)
-        {
-            Some((_sha1, dest, src)) => {
-                let mut source = fs::File::open(Path::new(src)).unwrap();
-                zip.start_file(dest, FileOptions::default()).unwrap();
-                std::io::copy(&mut source, &mut zip).unwrap();
+
+    let found: Vec<(String, PathBuf)> = bundle
+        .files
+        .iter()
+        .filter_map(|(sha, _file)| {
+            match bundle
+                .matches
+                .iter()
+                .find(|(sha1, _dest, _src)| sha == sha1)
+            {
+                Some((_sha1, dest, src)) => Some((dest.to_string(), src.to_path_buf())),
+                None => None,
             }
-            None => (),
-        }
-    });
-    zip.finish().unwrap();
+        })
+        .collect();
+    if !found.is_empty() {
+        let output =
+            fs::File::create(&path).unwrap_or_else(|_| panic!("Couldn't create {:?}", &path));
+        let mut zip = ZipWriter::new(output);
+        found.iter().for_each(|(dest, src)| {
+            let mut source =
+                fs::File::open(Path::new(src)).unwrap_or_else(|_| panic!("Couldn't open {:?}", src));
+            zip.start_file(dest, FileOptions::default())
+                .unwrap_or_else(|_| panic!("Couldn't start zip: {:?}", dest));
+            std::io::copy(&mut source, &mut zip)
+                .unwrap_or_else(|_| panic!("Couldn't copy into zip: {:?}", src));
+        });
+        zip.finish()
+            .unwrap_or_else(|_| panic!("Unable to finish zipfile: {:?}", output_file_name));
+    }
 }
 
 pub fn write_all_zip(bundles: Vec<rom::Bundle>, zip_dest: &PathBuf) {
