d2474e31bcb1628bd4f93a29ade72a9b6288cf86
Michael J. Cohen <mjc@kernel.org>
2022-01-01 17:32:47 -0700

    make models directory

---
+++ src/models/data_file.rs
@@ -0,0 +1,18 @@
+use crate::schema::data_files;
+
+#[derive(Identifiable, Queryable, Associations, PartialEq, Debug)]
+pub struct DataFile {
+    id: i32,
+    build: Option<String>,
+    debug: Option<String>,
+    file_name: Option<String>,
+    name: String,
+    description: Option<String>,
+    category: Option<String>,
+    version: Option<String>,
+    author: Option<String>,
+    email: Option<String>,
+    homepage: Option<String>,
+    url: Option<String>,
+    sha1: Vec<u8>,
+}
