c4662a4d2095ffac442a6bf65205385032d3f44a
Michael J. Cohen <mjc@kernel.org>
2022-05-22 22:11:12 -0600

    better logging

--- src/bin/mame_coalesce_server/main.rs
+++ src/bin/mame_coalesce_server/main.rs
@@ -2,11 +2,13 @@ use axum::{extract::Extension, routing::post, Json, Router};
 
 use hyper::StatusCode;
 use log::info;
-use mame_coalesce::{db, logiqx, operations::scan, MameResult};
+use mame_coalesce::{db, logger, logiqx, operations::scan, MameResult};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() -> MameResult<()> {
+    logger::setup_logger();
+
     let pool = db::create_sync_pool("coalesce.db")?;
 
     // build our application with a route
@@ -42,19 +44,23 @@ async fn scan_source(
     path: String,
     Extension(pool): Extension<db::SyncPool>,
 ) -> Result<Json<&'static str>, hyper::StatusCode> {
-    let conn = &mut pool
-        .get()
-        .or_else(|_| Err(StatusCode::INTERNAL_SERVER_ERROR))?;
+    let conn = &mut pool.get().or_else(|err| {
+        log::error!("database connection error: {:?}", err);
+        Err(StatusCode::INTERNAL_SERVER_ERROR)
+    })?;
     let file_list = scan::walk_for_files(camino::Utf8Path::new(path.as_str()));
-    let new_rom_files = scan::get_all_rom_files(&file_list, 16)
-        .or_else(|_| Err(StatusCode::INTERNAL_SERVER_ERROR))?;
+    let new_rom_files = scan::get_all_rom_files(&file_list, 16).or_else(|err| {
+        log::error!("failed to get all rom files: {:?}", err);
+        Err(StatusCode::INTERNAL_SERVER_ERROR)
+    })?;
 
     info!(
         "rom files found (unpacked and packed both): {}",
         new_rom_files.len()
     );
-    db::import_rom_files(conn, &new_rom_files)
-        .or_else(|_| Err(StatusCode::INTERNAL_SERVER_ERROR))?;
-
+    db::import_rom_files(conn, &new_rom_files).or_else(|err| {
+        log::error!("failed to import rom files: {:?}", err);
+        Err(StatusCode::INTERNAL_SERVER_ERROR)
+    })?;
     Ok(Json("moo"))
 }
