bd5540f220218078421f40340fa89e41619b353a
Mika Cohen <mjc@kernel.org>
2026-04-22 15:29:26 -0600

    chore: avoid logger setup panic

--- src/logger.rs
+++ src/logger.rs
@@ -2,11 +2,12 @@ use log::LevelFilter;
 use simplelog::{CombinedLogger, TermLogger};
 
 pub fn setup() {
-    let logger_result = CombinedLogger::init(vec![TermLogger::new(
+    if let Err(error) = CombinedLogger::init(vec![TermLogger::new(
         LevelFilter::Info,
         simplelog::Config::default(),
         simplelog::TerminalMode::Mixed,
         simplelog::ColorChoice::Never,
-    )]);
-    assert!(logger_result.is_ok(), "Unable to start logger");
+    )]) {
+        eprintln!("Unable to start logger: {error}");
+    }
 }
